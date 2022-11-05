65a6023e40168963fe785a3a81221bdeca178c3d
Yuumi0221 <chpp003@163.com>
2022-11-05 22:39:33 +0800

    添加图标

--- myCypher/main.cpp
+++ myCypher/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     Widget w;
     w.setWindowTitle("Yuumi's Cypher");
-    w.setWindowIcon(QIcon(":Yuumi.ico"));
+    w.setWindowIcon(QIcon(":yuumi.ico"));
     w.show();
     return a.exec();
 }
